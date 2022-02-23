d82f52bffad0298f55eb18c729242f472fa2443a
thomascd9 <Thomas.Chang_davidson@tufts.edu>
2022-02-22 18:02:13 -0800

    made basic HTML/CSS version

--- dash-gui/src/components/Picture/Picture.js
+++ dash-gui/src/components/Picture/Picture.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './Picture.css';
 
-function App() {
+
+function Picture() {
+  // have list of all image names
+  // pick a random one (js)
+  // set src to that variable
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img src="./testimages/960x0.jpg" alt="fuck react"/>
   );
 }
 
-export default App;
+export default Picture;
