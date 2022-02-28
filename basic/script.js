95289089f8b30f324c319c5362dfd49e88498216
thomascd9 <Thomas.Chang_davidson@tufts.edu>
2022-02-28 13:21:26 -0800

    added pausing, picture name, & reloading iframes

--- basic/script.js
+++ basic/script.js
@@ -1,6 +1,8 @@
 // Images to draw from (for testing):
-const images = ["960x0.jpg", "blue.jpg"];
+const test_imgs = ["960x0.jpg", "blue.jpg"];
 // Actual image names are in image_names.js in const all_img_names
+
+var paused = false;
 
 function startTime() {
   const today = new Date();
@@ -11,16 +13,32 @@ function startTime() {
   s = checkTime(s);
   document.getElementById('clock').innerText =  h + ":" + m + ":" + s;
 
+
   document.getElementById('date').innerText = today.toDateString();
 
   const imgChangeInterval = 10; // change images every x seconds
-  if (today.getSeconds() % imgChangeInterval == 0) {
+  if (today.getSeconds() % imgChangeInterval == 0 && !paused) {
     let rand_img = all_img_names[Math.floor(Math.random() * all_img_names.length)];
     document.body.style.backgroundImage = "url('" + rand_img + "')";
+    document.getElementById('imagename').innerText = rand_img;
   }
+
+  const weatherUpdateInterval = 10; // upedate weather and aqi every x minutes
+  if (today.getMinutes() % weatherUpdateInterval == 0) {
+      document.getElementById('aqi').src += '';
+      document.getElementById('weather').src += '';
+  }
+
 
   setTimeout(startTime, 1000);
 }
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'p' || event.key == ' ') {
+        paused = !paused
+        console.log('paused = ' + paused);
+    }
+})
 
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
